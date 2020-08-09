df3aa4107836ee00ea8094739996f25c41eaa701
Oskar Enmalm <oskarenmalm@gmail.com>
2020-08-09 14:06:28 +0200

    Downside risk calculation with semideviation, VaR, and CVaR

--- 1/my_modules/risk.py
+++ 1/my_modules/risk.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import scipy.stats
+import numpy as np
+import scipy.stats as stats
 
 def drawdown(return_series: pd.Series):
     """
@@ -45,6 +46,61 @@ def get_hfi_returns():
     hfi.index = hfi.index.to_period('M')
     return hfi
   
+def semideviation(r):
+  """
+  Calculates the semideviation (the semideviation of negative returns) of r where r is
+  a Series or a DataFrame
+  """
+  return r[r<0].std(ddof=0)
+
+def var_historic(r, level=5):
+  """
+  VaR Historic
+  When using the numpy array we lose the column information
+  """
+  # Calls the function again but with every individual Series
+  if isinstance(r, pd.DataFrame):
+    return r.aggregate(var_historic, level=level)
+  elif isinstance(r, pd.Series):
+    return -np.percentile(r, level)
+  else: 
+    raise TypeError("r is expected to be a Series or DataFrame")
+    
+def cvar_historic(r, level=5):
+  """
+  CVaR Historic
+  Adaptation of var_historic to isolate the returns that are less than the mean
+  """
+  # Calls the function again but with every individual Series
+  if isinstance(r, pd.DataFrame):
+    return r.aggregate(cvar_historic, level=level)
+  elif isinstance(r, pd.Series):
+    is_beyond = r <= -var_historic(r, level)
+    return -r[is_beyond].mean()
+  else: 
+    raise TypeError("r is expected to be a Series or DataFrame")
+  
+def var_gaussian(r, level=5, modified=False):
+  """
+  VaR Gaussian
+  """
+  z = stats.norm.ppf(level/100)
+  
+  if(modified):
+    # Calculate the z score based on observed skewness and kurtosis
+    s = skewness(r)
+    k = kurtosis(r)
+    z = (z + 
+          (z**2 - 1)*s/6 + 
+          (z**3 -3*z)*(k-3)/24 -
+          (2*z**3 - 5*z)*(s**2)/36
+        )
+  
+  return -(r.mean() + z * r.std(ddof=0))
+
+def var_cornish_fisher(r, level=5):
+  return var_gaussian(r, level, True)
+  
 def skewness(returns):
   """
   Manual implementation of scipy.stats.skew()
@@ -71,7 +127,7 @@ def __skew_kurtosis(returns, power_to_raise):
 
 def is_normal(r, level=0.01):
   """
-  Applies the Jar
+  Applies the Jarque Bera Test
   """
   statistic, p_value = scipy.stats.jarque_bera(r)
   return p_value > level
