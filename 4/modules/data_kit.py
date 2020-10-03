7bc40a279abb7e2de924a7b22f619ae05ec26bec
Oskar Enmalm <oskarenmalm@gmail.com>
2020-10-04 00:27:23 +0200

    403
    Using the mapping functionality of folium to visualise the data

--- 4/modules/data_kit.py
+++ 4/modules/data_kit.py
@@ -1,4 +1,5 @@
 from math import radians, cos, sin, asin, sqrt
+import folium as folium
 
 def haversine(coordinates1, coordinates2):
     """Takes two coordinates and uses the Haversine equation to calculate the distance"""
@@ -18,3 +19,15 @@ def haversine(coordinates1, coordinates2):
     c = 2 * asin(sqrt(a))
     r = 3956
     return c * r
+  
+def get_centered_nyc_map():
+  return folium.Map(location=[40.7128, -74.006], zoom_start=12)
+
+def add_distance_to_poi_col(dataset, point_of_interest_coords, title, lat_header='Lat', lon_header='Lon'):
+  """
+  Takes a DataSet containing the cols 'Lat' and 'Lon'.
+  Calculates the distance for each rows Lat and Lon with a given (Lat,Lon) Tuple
+  Returns the DataSet with an additional column representing the distance to the Tuple
+  """
+  dataset[title] = dataset[[lat_header, lon_header]].apply(lambda x: haversine(point_of_interest_coords, tuple(x)), axis=1)
+  return dataset
